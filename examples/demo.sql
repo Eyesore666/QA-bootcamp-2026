05da143adc1ad80838acaf592ea66f2f313029df
Eyesore666 <7e4enbka@mail.ru>
2026-03-20 16:20:39 +0300

    Add examples/demo.sql. reports/gitkeep and requirements.txt

---
+++ examples/demo.sql
@@ -0,0 +1,2 @@
+﻿CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT NOT NULL, last_name TEXT NOT NULL, email TEXT UNIQUE NOT NULL, created_at TEXT DEFAULT CURRENT_TIMESTAMP);
+INSERT INTO users (first_name, last_name, email) VALUES ('Ivan','Ivanov','ivan@example.com');
